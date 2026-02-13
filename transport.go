1747dc4bee5ce139f7c531a897df671283889fc0
AMIR <amir6yn@gmail.com>
2026-02-13 22:21:41 +0330

    New Update

--- transport.go
+++ transport.go
@@ -156,6 +156,9 @@ func (hc *HTTPConn) RoundTrip(payload []byte) ([]byte, error) {
 		for _, c := range resp.Cookies() {
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> eac5513 (New Update)
 =======
 >>>>>>> eac5513 (New Update)
 			if c == nil || c.Value == "" {
@@ -169,12 +172,15 @@ func (hc *HTTPConn) RoundTrip(payload []byte) ([]byte, error) {
 			}
 			if c.Name == hc.CookieName {
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 			if c == nil {
 				continue
 			}
 			if c.Name == "session" && c.Value != "" {
 >>>>>>> ce9c9960a462af1d2b04906222b2d63c6bdd40fd
+=======
+>>>>>>> eac5513 (New Update)
 =======
 >>>>>>> eac5513 (New Update)
 				hc.SessionID = c.Value
